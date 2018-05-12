e10dca35e4a08050581004b1210ef51bc5420462
Gregg Lebovitz <gregg@lebovitz.net>
2018-05-12 12:38:45 -0400

    add redux; add initial data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
-ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt:18000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 400, createdAt: 15000 }));
+store.dispatch(addExpense({ description: 'rent', amount: 100, createdAt: -21000}));
+store.dispatch(addExpense({ description: 'coffee', amount: 300, createdAt: -1000}));
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />  
+  </Provider>
+
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
