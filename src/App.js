6d8b0a2249df01a340c47b3e93315e685b0d4ed9
Gregg Lebovitz <gregg@lebovitz.net>
2018-05-16 12:26:46 -0400

    set up test database env

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt:18000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 400, createdAt: 15000 }));
-store.dispatch(addExpense({ description: 'rent', amount: 100, createdAt: -21000}));
-store.dispatch(addExpense({ description: 'coffee', amount: 300, createdAt: -1000}));
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt:18000 }));
+// store.dispatch(addExpense({ description: 'Gas Bill', amount: 400, createdAt: 15000 }));
+// store.dispatch(addExpense({ description: 'rent', amount: 100, createdAt: -21000}));
+// store.dispatch(addExpense({ description: 'coffee', amount: 300, createdAt: -1000}));
 
 const jsx = (
   <Provider store={store}>
